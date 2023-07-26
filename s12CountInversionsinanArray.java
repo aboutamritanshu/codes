a78ea16804083fe649bd445380cfc083a075f639
aboutamritanshu <132550606+aboutamritanshu@users.noreply.github.com>
2023-07-26 23:29:08 +0530

    s12CountInversionsinanArray.java

---
+++ s12CountInversionsinanArray.java
@@ -0,0 +1,59 @@
+
+import java.util.*;
+public class noofinversions 
+{
+    private static int merge(int[] arr, int low, int mid, int high) 
+  {
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int left = low;      
+        int right = mid + 1;   
+        int cnt = 0;
+        while (left <= mid && right <= high)
+          {
+            if (arr[left] <= arr[right]) 
+            {
+                temp.add(arr[left]);
+                left++;
+            } else 
+            {
+                temp.add(arr[right]);
+                cnt += (mid - left + 1); 
+                right++;
+            }
+          }
+        while (left <= mid)
+          {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return cnt; // Modification 3
+    }
+    public static int mergeSort(int[] arr, int low, int high)
+  {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  
+        cnt += mergeSort(arr, mid + 1, high); 
+        cnt += merge(arr, low, mid, high);  
+        return cnt;
+    }
+    public static int numberOfInversions(int[] a, int n)
+  {
+        return mergeSort(a, 0, n - 1);
+    }
+    public static void main(String[] args)
+  {
+        int[] a = {5, 4, 3, 2, 1};
+        int n = 5;
+        int cnt = numberOfInversions(a, n);
+        System.out.println("The number of inversions are: " + cnt);
+    }
+}
